5f1eaa21d4b38a53b071a407e9f42cf8fbaf9a73
poly000 <1348292515@qq.com>
2023-05-20 17:43:58 +0800

    todo: ScannerArgs Bridge

--- ffi/ptrsx.h
+++ ffi/ptrsx.h
@@ -1,13 +1,20 @@
 typedef struct PtrsX PtrsX;
 
+typedef struct Addr {
+  const void *start;
+  const void *end;
+  const char *path;
+} Addr;
+
 struct PtrsX *ptrsx_init(int pid);
 
 void ptrsx_free(struct PtrsX *ptr);
 
 int ptrsx_create_pointer_map(struct PtrsX *ptr, const char *path);
 
-typedef void* (*addr)[2];
-addr ptrsx_load_pointer_map(struct PtrsX *ptr, const char *path, int *length);
+const struct Addr *ptrsx_load_pointer_map(struct PtrsX *ptr,
+                                          const char *path,
+                                          unsigned int *length);
 
 int last_error_length(void);
 
