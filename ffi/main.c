9c305c4b06458ed2357fff8491705b9279505718
poly000 <1348292515@qq.com>
2023-05-21 22:36:09 +0800

    add example driver for ffi api

--- ffi/main.c
+++ ffi/main.c
@@ -2,4 +2,36 @@
 #include <stdint.h>
 #include <stdio.h>
 
-int main() { return 0; }
+int main() {
+  int pid;
+  scanf("%d", &pid);
+
+  PtrsX *const ptrsx = ptrsx_init(pid);
+  if (!ptrsx) {
+    return -1;
+  }
+
+  ptrsx_create_pointer_map(ptrsx, "test.map");
+  unsigned int region_size;
+  const Addr *addrs = ptrsx_load_pointer_map(ptrsx, "test.map", &region_size);
+
+  if (!addrs) {
+    printf("pointer map load failed!\n");
+    return -1;
+  } else {
+    printf("pointer map loaded!\n");
+  }
+
+  uintptr_t target_addr;
+  scanf("%ju", &target_addr);
+
+  int status = ptrsx_scan_pointer_path(ptrsx, "test", addrs, region_size, NULL,
+                                       7, target_addr, 0, 600);
+  if (-3 == status) {
+    int error_len = last_error_length();
+    char error_msg[error_len];
+    last_error_message(error_msg, error_len);
+    printf("error occured in scanning: %s\n", error_msg);
+  }
+  ptrsx_free(ptrsx);
+}
