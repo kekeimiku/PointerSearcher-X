4279f6f4624fabb6a1fd68ee9ae2c84ee5318ff3
kekeimiku <kekelanact@gmail.com>
2023-07-19 09:26:54 +0800

    refactor vmmap error

--- vmmap/src/error.rs
+++ vmmap/src/error.rs
@@ -20,15 +20,45 @@ pub enum Error {
     WriteMemory(windows_sys::Win32::Foundation::WIN32_ERROR),
 }
 
+#[cfg(target_os = "macos")]
+#[inline]
+pub unsafe fn mach_error(error_value: machx::error::mach_error_t) -> String {
+    let ptr = machx::error::mach_error_string(error_value);
+    String::from_utf8_unchecked(std::ffi::CStr::from_ptr(ptr).to_bytes().to_owned())
+}
+
+#[cfg(target_os = "macos")]
 impl std::fmt::Display for Error {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        let s = match self {
-            Error::OpenProcess(e) => format!("OpenProcess {e}"),
-            Error::ReadMemory(e) => format!("OpenProcess {e}"),
-            Error::WriteMemory(e) => format!("WriteMemory {e}"),
-        };
+        unsafe {
+            match self {
+                Error::OpenProcess(err) => write!(f, "{}. code: {err}", mach_error(*err)),
+                Error::ReadMemory(err) => write!(f, "{}. code: {err}", mach_error(*err)),
+                Error::WriteMemory(err) => write!(f, "{}. code: {err}", mach_error(*err)),
+            }
+        }
+    }
+}
 
-        write!(f, "{}", s)
+#[cfg(target_os = "linux")]
+impl std::fmt::Display for Error {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            Error::OpenProcess(err) => write!(f, "{err}"),
+            Error::ReadMemory(err) => write!(f, "{err}"),
+            Error::WriteMemory(err) => write!(f, "{err}"),
+        }
+    }
+}
+
+#[cfg(target_os = "windows")]
+impl std::fmt::Display for Error {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            Error::OpenProcess(err) => write!(f, "{err}"),
+            Error::ReadMemory(err) => write!(f, "{err}"),
+            Error::WriteMemory(err) => write!(f, "{err}"),
+        }
     }
 }
 
